797f304008f4eff55576321013349021e462df12
Jason Klipple <jasonklipple@Jasons-MBP.lan>
2022-01-06 01:23:54 -0600

    Updated commit

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,12 +1,20 @@
+import { UserProps } from './User';
+
 export class Attributes<T> {
 
     constructor(private data: T) { }
 
-    get(propName: string): any {
-        return this.data[propName];
-    }
+    // K can only be elements of T. Take whatever K is in T and return its respective type
+    get = <K extends keyof T>(key: K): T[K] => {
+        return this.data[key];
+    };
 
     set(update: T): void {
         Object.assign(this.data, update)
     }
+
+    getAll(): T {
+        return this.data;
+    }
+
 }
